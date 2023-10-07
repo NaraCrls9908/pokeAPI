7905ca0bed239fd028c7500d5defe15c234a5c01
carlos <cdelgado@penstockconsulting.com>
2023-10-06 21:39:54 -0600

    corrections in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import './App.css'
 import { useFetch } from './hooks/useFetch'
 import PokemonDetails from './components/PokemonDetails/pokemonDetails'
 
 function App() {
-  const [pokeData, getAPI] = useFetch();
-  const [pokemonName, setPokemonName] = useState('');
+  const [pokemons, getAPI] = useFetch();
   const url = 'https://pokeapi.co/api/v2/pokemon';
   useEffect(() => {
     getAPI(url);
   }, [])
   
-  if(pokeData) {
-    console.log("first=>", pokeData
-    );
-  }
   return (
     <>
-       <ul className="pokemon-list">
-        {pokeData?.map((pokemon, index) => (
-          <><PokemonDetails url={pokemon.url} /></>
+      <ul className="pokemon-list">
+      {pokemons?.map((pokemon, index) => (
+          <li key={index} className="pokemon-item">
+            <PokemonDetails url={pokemon.url} />
+          </li>
         ))}
       </ul>
     </>
